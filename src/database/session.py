4c399c7cfa65e9ce4f807ebe49e04902eb483bb4
4ikkamony <compbanderloga@gmail.com>
2025-03-21 16:10:16 +0200

    Move import to the top of the file

--- src/database/session.py
+++ src/database/session.py
@@ -3,8 +3,8 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
+from database.models import accounts, movies
 from database.models.base import Base
-
 
 BASE_DIR = Path(__file__).parent.parent
 PATH_TO_DB = str(BASE_DIR / "database" / "cinema.db")
@@ -28,9 +28,6 @@ async def init_db() -> None:
     This function creates all tables defined in the SQLAlchemy ORM models.
     It should be called at the application startup to ensure that the database schema exists.
     """
-    from database.models import accounts, movies
-
-
     async with sqlite_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
