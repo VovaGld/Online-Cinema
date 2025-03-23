f29d80dff670d5b2f3ecfd6d8ea89cbc65ba3e03
4ikkamony <compbanderloga@gmail.com>
2025-03-23 15:27:48 +0200

    Update shopping_cart dependencies
    Add get_shopping_cart_item_repository
    and get_shopping_cart_service dependencies

--- src/dependencies/shopping_cart.py
+++ src/dependencies/shopping_cart.py
@@ -2,10 +2,26 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.session import get_db
+from repositories.cart_item_rep import CartItemRepository
 from repositories.shopping_cart_rep import ShoppingCartRepository
+from services.shopping_cart import ShoppingCartService
 
 
 def get_shopping_cart_repository(
     session: AsyncSession = Depends(get_db),
 ) -> ShoppingCartRepository:
     return ShoppingCartRepository(session=session)
+
+def get_shopping_cart_item_repository(
+    session: AsyncSession = Depends(get_db),
+) -> CartItemRepository:
+    return CartItemRepository(session=session)
+
+def get_shopping_cart_service(
+    shopping_cart_repository = Depends(get_shopping_cart_repository),
+    cart_item_repository = Depends(get_shopping_cart_item_repository),
+) -> ShoppingCartService:
+    return ShoppingCartService(
+        shopping_cart_repository=shopping_cart_repository,
+        cart_item_repository=cart_item_repository,
+    )
