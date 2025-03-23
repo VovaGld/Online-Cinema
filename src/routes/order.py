c0566afda7fdd58af34e0372569c5998c30a90b0
4ikkamony <compbanderloga@gmail.com>
2025-03-23 19:00:45 +0200

    Comment old import out, because interface of OrderRepository changed

--- src/routes/order.py
+++ src/routes/order.py
@@ -3,29 +3,29 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.session import get_db
-from repositories.order_rep import create_order, get_movie_from_card
+# from repositories.order_rep import create_order, get_movie_from_card
 from schemas.order import OrderCreateResponseSchema
 
 router = APIRouter()
 
-@router.post("/create/")
-async def create(
-    db: AsyncSession = Depends(get_db)
-) -> OrderCreateResponseSchema:
-    try:
-        order = await create_order(
-            db=db,
-            user_id=1,
-            movies= await get_movie_from_card(
-                user_id=1,
-                db=db
-            )
-        )
-    except SQLAlchemyError as e:
-        raise e
-    return OrderCreateResponseSchema(
-        order_id=order.id,
-        total_price=order.total_amount,
-        status=order.status,
-        payment_url="some url",
-    )
+# @router.post("/create/")
+# async def create(
+#     db: AsyncSession = Depends(get_db)
+# ) -> OrderCreateResponseSchema:
+#     try:
+#         order = await create_order(
+#             db=db,
+#             user_id=1,
+#             movies= await get_movie_from_card(
+#                 user_id=1,
+#                 db=db
+#             )
+#         )
+#     except SQLAlchemyError as e:
+#         raise e
+#     return OrderCreateResponseSchema(
+#         order_id=order.id,
+#         total_price=order.total_amount,
+#         status=order.status,
+#         payment_url="some url",
+#     )
