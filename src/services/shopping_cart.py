4f52908d81c86cd3e9458b2e36a74362101b89f1
4ikkamony <compbanderloga@gmail.com>
2025-03-24 15:10:14 +0200

    Update ShoppingCartService
    Add get_cart_item_detail method - get a single item from cart
    
    Add raising HTTPException if movie already in cart in add_movie_to_cart
    method

--- src/services/shopping_cart.py
+++ src/services/shopping_cart.py
@@ -10,7 +10,7 @@ from schemas.shopping_cart import (
     CartItemDetailSchema,
 )
 from security.interfaces import JWTAuthManagerInterface
-
+from exceptions.cart_item import CartItemAlreadyInCartError
 
 class ShoppingCartService:
     def __init__(
@@ -34,6 +34,7 @@ class ShoppingCartService:
         cart = await self.shopping_cart_repository.get_or_create_cart(user_id)
         items = await self.get_cart_items_details(cart)
         response = CartResponseSchema(
+            id=cart.id,
             user_id=user_id,
             items=items
         )
@@ -53,13 +54,31 @@ class ShoppingCartService:
             for item in cart.items
         ] if cart else []
 
+    async def get_cart_item_detail(self, item: CartItemModel) -> CartItemDetailSchema:
+        return CartItemDetailSchema(
+                id=item.id,
+                movie_id=item.movie.id,
+                title=item.movie.name,
+                price=item.movie.price,
+                genres=[genre.name for genre in item.movie.genres],
+                release_year=item.movie.year,
+                warning=None,
+            )
+
     async def get_or_create_cart(self, user_id: int):
         cart = await self.shopping_cart_repository.get_or_create_cart(user_id)
         return cart
 
-    async def add_movie_to_cart(self, cart_id: int, movie_id) -> CartItemModel:
-        new_item = await self.cart_item_repository.create_cart_item(cart_id, movie_id)
-        return new_item
+    async def add_movie_to_cart(self, cart_id: int, movie_id) -> CartItemDetailSchema:
+        try:
+            new_item = await self.cart_item_repository.create_cart_item(cart_id, movie_id)
+        except CartItemAlreadyInCartError:
+            raise HTTPException(
+                status_code=400,
+                detail="Movie already in cart."
+            )
+        response = await self.get_cart_item_detail(new_item)
+        return response
 
     async def clear_cart(self, cart_id: int) -> None:
         await self.cart_item_repository.delete_all_cart_items(cart_id)
