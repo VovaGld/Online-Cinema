538dd81546c85c2f100000d8135a69b804638179
4ikkamony <compbanderloga@gmail.com>
2025-03-24 13:00:49 +0200

    Add get_user_cart to ShoppingCartService
    It returns the current user's cart

--- src/services/shopping_cart.py
+++ src/services/shopping_cart.py
@@ -1,6 +1,15 @@
-from database.models.shopping_cart import CartItemModel
+from typing import List
+
+from fastapi import HTTPException, status
+
+from database.models.shopping_cart import CartItemModel, CartModel
 from repositories.shopping_cart_rep import ShoppingCartRepository
 from repositories.cart_item_rep import CartItemRepository
+from schemas.shopping_cart import (
+    CartResponseSchema,
+    CartItemDetailSchema,
+)
+from security.interfaces import JWTAuthManagerInterface
 
 
 class ShoppingCartService:
@@ -11,6 +20,38 @@ class ShoppingCartService:
     ) -> None:
         self.shopping_cart_repository = shopping_cart_repository
         self.cart_item_repository = cart_item_repository
+
+    async def get_user_cart(self, token: str, jwt_manager: JWTAuthManagerInterface) -> CartResponseSchema:
+        try:
+            payload = jwt_manager.decode_access_token(token)
+            user_id = payload.get("user_id")
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=str(e)
+            )
+
+        cart = await self.shopping_cart_repository.get_or_create_cart(user_id)
+        items = await self.get_cart_items_details(cart)
+        response = CartResponseSchema(
+            user_id=user_id,
+            items=items
+        )
+        return response
+
+    async def get_cart_items_details(self, cart: CartModel) -> List[CartItemDetailSchema]:
+        return [
+            CartItemDetailSchema(
+                id=item.id,
+                movie_id=item.movie.id,
+                title=item.movie.name,
+                price=item.movie.price,
+                genres=[genre.name for genre in item.movie.genres],
+                release_year=item.movie.year,
+                warning=None,
+            )
+            for item in cart.items
+        ] if cart else []
 
     async def get_or_create_cart(self, user_id: int):
         cart = await self.shopping_cart_repository.get_or_create_cart(user_id)
