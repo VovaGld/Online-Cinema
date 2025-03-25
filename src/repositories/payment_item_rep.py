ef62fef56c01b5d93b893ae59f360026fe67d4f3
Vova <vova.gld05@gmail.com>
2025-03-25 17:22:13 +0200

    Add PaymentItemRepository for handling payment item creation

---
+++ src/repositories/payment_item_rep.py
@@ -0,0 +1,28 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.models import OrderItemModel
+
+
+class PaymentItemRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_payment_items(self, payment_id: int, order_items: list[OrderItemModel]) -> None:
+        try:
+            payment_items = []
+            for item in order_items:
+
+                payment_items.append(
+                    PaymentItemModel(
+                        payment_id=payment_id,
+                        order_item_id=item.id,
+                        price_at_payment=item.price_at_order
+                    )
+                )
+
+            self.session.add_all(order_items)
+            await self.session.commit()
+        except SQLAlchemyError as e:
+            await self.session.rollback()
+            raise e
